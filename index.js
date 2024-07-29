16e5e9b3f9982b28abff645a92ce1025f128c650
Wilson Lopez <wilson1lopezmercado@gmail.com>
2024-07-28 20:14:03 -0500

    Actualización en index.js

--- index.js
+++ index.js
@@ -3,27 +3,47 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json()); // Para manejar cuerpos JSON
 
 // Configuración de la conexión a MySQL
-const connection = mysql.createConnection({
-  host: 'booxfva7gkco41ueigpq-mysql.services.clever-cloud.com',
-  user: 'u9ogds8egmwvevci',
-  password: 'Gbajf9DNtghlzmc5lplR', // Reemplaza esto con tu contraseña de MySQL
-  database: 'booxfva7gkco41ueigpq'
-});
+let connection;
 
-// Intentar conectar a la base de datos
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.message);
-  } else {
-    console.log('Connection to the database was successful!');
-  }
-});
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    host: 'booxfva7gkco41ueigpq-mysql.services.clever-cloud.com',
+    user: 'u9ogds8egmwvevci',
+    password: 'Gbajf9DNtghlzmc5lplR', // Reemplaza esto con tu contraseña de MySQL
+    database: 'booxfva7gkco41ueigpq'
+    // Para desarrollo local
+    // host: 'localhost',
+    // user: 'root',
+    // password: '', // Reemplaza esto con tu contraseña de MySQL
+    // database: 'debtors_db'
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err.message);
+      setTimeout(handleDisconnect, 2000); // Intentar reconectar después de 2 segundos
+    } else {
+      console.log('Connection to the database was successful!');
+    }
+  });
+
+  connection.on('error', (err) => {
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect(); // Reconectar en caso de pérdida de conexión
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 // Rutas de tu aplicación
 app.get('/debtors', (req, res) => {
